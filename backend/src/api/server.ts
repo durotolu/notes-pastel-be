a1794e6f2417abda6dee8fa0e11603f2ee4696c0
durotolu <durolawk@gmail.com>
2024-08-31 23:03:51 +0100

    resolved cors issue and restricted to one endpoint

--- backend/src/api/server.ts
+++ backend/src/api/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import noteRoutes from '../routes/noteRoutes';
 
 const server = express();
+
+server.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 server.use(express.json());
 server.use('/api', noteRoutes);
